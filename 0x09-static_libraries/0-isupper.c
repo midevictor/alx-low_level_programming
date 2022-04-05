4ecc019935d0c6553de3240c9eec3d27c5dd4885
Ayomide Victor <midevictor90@gmail.com>
2022-04-05 13:32:48 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,18 +2,18 @@
 
 /**
  * _isupper - checks whether a character is uppercase.
- * @c: the integer to be supplied by user.
+ *@c: the integer to be supplied by user.
  * Return: 0 or 1.
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
