b8057c043617ad849424a29273a615955dec7ffa
midevictor <midevictor90@gmail.com>
2022-06-16 08:56:49 -0700

    added files

---
+++ 0x18-dynamic_libraries/operational.c
@@ -0,0 +1,54 @@
+/**
+ * add - returns the sum of two integers
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns the subtraction of the two numbers
+ * @a: integer
+ * @b: integer
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul -returns the subtraction of the two numbers
+ * @a: integer
+ * @b: integer
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -returns the divisio  of the two numbers
+ * @a: integer
+ * @b: integer
+ * Return: div
+ */
+int div(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod  -returns the mod of the two numbers
+ * @a: integer
+ * @b: integer
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
